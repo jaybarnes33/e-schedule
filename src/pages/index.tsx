1000de9dcc141839daa9afa863d34d8652d5bd93
Dchole <derekoware47@gmail.com>
2021-06-29 23:54:14 +0000

    firebase config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
+import Head from "next/head"
+import Image from "next/image"
+import Header from "@/components/Header"
 
 export default function Home() {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  )
 }
