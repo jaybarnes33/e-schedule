d5850e662e682c6c856326433d6df3f50f52f340
Dchole <derekoware47@gmail.com>
2021-07-27 12:34:39 +0000

    upgrade node packages

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -4,54 +4,68 @@ import { useRouter } from "next/router"
 import NextLink, { LinkProps as NextLinkProps } from "next/link"
 import MuiLink, { LinkProps as MuiLinkProps } from "@material-ui/core/Link"
 
-type NextComposedProps = Omit<
-  React.AnchorHTMLAttributes<HTMLAnchorElement>,
-  "href"
-> &
-  NextLinkProps
-
-const NextComposed = forwardRef<HTMLAnchorElement, NextComposedProps>(
-  (props, ref) => {
-    const { as, href, replace, scroll, passHref, shallow, prefetch, ...other } =
-      props
-
-    return (
-      <NextLink
-        href={href}
-        prefetch={prefetch}
-        as={as}
-        replace={replace}
-        scroll={scroll}
-        shallow={shallow}
-        passHref={passHref}
-      >
-        <a ref={ref} {...other} />
-      </NextLink>
-    )
-  }
-)
-
-NextComposed.displayName = "NextComposed"
-
-interface LinkPropsBase {
-  activeClassName?: string
-  innerRef?: React.Ref<HTMLAnchorElement>
-  naked?: boolean
+interface NextLinkComposedProps
+  extends Omit<React.AnchorHTMLAttributes<HTMLAnchorElement>, "href">,
+    Omit<NextLinkProps, "href" | "as"> {
+  to: NextLinkProps["href"]
+  linkAs?: NextLinkProps["as"]
+  href?: NextLinkProps["href"]
 }
 
-export type LinkProps = LinkPropsBase &
-  NextComposedProps &
+export const NextLinkComposed = forwardRef<
+  HTMLAnchorElement,
+  NextLinkComposedProps
+>(function NextLinkComposed(props, ref) {
+  const {
+    to,
+    linkAs,
+    href,
+    replace,
+    scroll,
+    passHref,
+    shallow,
+    prefetch,
+    locale,
+    ...other
+  } = props
+
+  return (
+    <NextLink
+      href={to}
+      prefetch={prefetch}
+      as={linkAs}
+      replace={replace}
+      scroll={scroll}
+      shallow={shallow}
+      passHref={passHref}
+      locale={locale}
+    >
+      <a ref={ref} {...other} />
+    </NextLink>
+  )
+})
+
+export type LinkProps = {
+  activeClassName?: string
+  as?: NextLinkProps["as"]
+  href: NextLinkProps["href"]
+  noLinkStyle?: boolean
+} & Omit<NextLinkComposedProps, "to" | "linkAs" | "href"> &
   Omit<MuiLinkProps, "href">
 
 // A styled version of the Next.js Link component:
 // https://nextjs.org/docs/#with-link
-function Link(props: LinkProps) {
+const Link = forwardRef<HTMLAnchorElement, LinkProps>(function Link(
+  props,
+  ref
+) {
   const {
-    href,
     activeClassName = "active",
+    as: linkAs,
     className: classNameProps,
-    innerRef,
-    naked,
+    href,
+    noLinkStyle,
+    role, // Link don't have roles.
     ...other
   } = props
 
@@ -61,12 +75,38 @@ function Link(props: LinkProps) {
     [activeClassName]: router.pathname === pathname && activeClassName
   })
 
-  if (naked) {
+  const isExternal =
+    typeof href === "string" &&
+    (href.indexOf("http") === 0 || href.indexOf("mailto:") === 0)
+
+  if (isExternal) {
+    if (noLinkStyle) {
+      return (
+        <a
+          className={className}
+          href={href as string}
+          ref={ref as any}
+          {...other}
+        />
+      )
+    }
+
     return (
-      <NextComposed
+      <MuiLink
         className={className}
-        ref={innerRef}
-        href={href}
+        href={href as string}
+        ref={ref}
+        {...other}
+      />
+    )
+  }
+
+  if (noLinkStyle) {
+    return (
+      <NextLinkComposed
+        className={className}
+        ref={ref as any}
+        to={href}
         {...other}
       />
     )
@@ -74,19 +114,14 @@ function Link(props: LinkProps) {
 
   return (
     <MuiLink
-      component={NextComposed}
+      component={NextLinkComposed}
+      linkAs={linkAs}
       className={className}
-      ref={innerRef}
-      href={href as string}
+      ref={ref}
+      to={href}
       {...other}
     />
   )
-}
+})
 
-const LinkRef = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => (
-  <Link {...props} innerRef={ref} />
-))
-
-LinkRef.displayName = "LinkRef"
-
-export default LinkRef
+export default Link
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,15 @@
-import { Children } from "react"
+import { CacheProvider } from "@emotion/react"
+import createCache from "@emotion/cache"
 import Document, { Html, Head, Main, NextScript } from "next/document"
-import { ServerStyleSheets } from "@material-ui/core/styles"
+import createEmotionServer from "@emotion/server/create-instance"
 import theme from "@/lib/theme"
+import { Children } from "react"
+
+function getCache() {
+  const cache = createCache({ key: "css", prepend: true })
+  cache.compat = true
+  return cache
+}
 
 export default class MyDocument extends Document {
   render() {
@@ -10,6 +18,10 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
         </Head>
         <body>
           <Main />
@@ -21,7 +33,7 @@ export default class MyDocument extends Document {
 }
 
 // `getInitialProps` belongs to `_document` (instead of `_app`),
-// it's compatible with server-side generation (SSG).
+// it's compatible with static-site generation (SSG).
 MyDocument.getInitialProps = async ctx => {
   // Resolution order
   //
@@ -45,20 +57,37 @@ MyDocument.getInitialProps = async ctx => {
   // 3. app.render
   // 4. page.render
 
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets()
   const originalRenderPage = ctx.renderPage
+
+  const cache = getCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
 
   ctx.renderPage = () =>
     originalRenderPage({
-      enhanceApp: App => props => sheets.collect(<App {...props} />)
+      // Take precedence over the CacheProvider in our custom _app.js
+      // eslint-disable-next-line react/display-name
+      enhanceComponent: Component => props =>
+        (
+          <CacheProvider value={cache}>
+            <Component {...props} />
+          </CacheProvider>
+        )
     })
 
   const initialProps = await Document.getInitialProps(ctx)
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map(style => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
 
   return {
     ...initialProps,
     // Styles fragment is rendered after the app and page rendering finish.
-    styles: [...Children.toArray(initialProps.styles), sheets.getStyleElement()]
+    styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags]
   }
 }
