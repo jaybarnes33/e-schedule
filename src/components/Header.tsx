38c9bf92c9481f61c6ece937267d74ee0bc6944e
Dchole <derekoware47@gmail.com>
2021-07-27 13:07:43 +0000

    fonts

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,15 @@
 import AppBar from "@material-ui/core/AppBar"
 import Toolbar from "@material-ui/core/Toolbar"
+import Typography from "@material-ui/core/Typography"
 
 const Header = () => {
   return (
-    <AppBar>
-      <Toolbar>Lorem</Toolbar>
+    <AppBar variant="outlined" color="inherit">
+      <Toolbar>
+        <Typography variant="h3" component="h1">
+          E-Schedule
+        </Typography>
+      </Toolbar>
     </AppBar>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import "@fontsource/roboto"
+import "@fontsource/montserrat/500.css"
+import "@fontsource/montserrat/600.css"
+import "@fontsource/montserrat/700.css"
 
 import Head from "next/head"
 import { AppProps } from "next/app"
